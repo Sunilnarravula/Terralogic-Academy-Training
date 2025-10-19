a44e5604bf82d478be61a8b34d846796bd79f2f1
sunilnarravula <sunilnarravula2001@gmail.com>
2025-10-19 09:15:44 +0530

    added placeholder

---
+++ frontend/reactsample/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Component from './components/Component';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+     <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/reactsample/src/components/Component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './Test';
+
+const Component = () => {
+  return (
+    <div>
+      <input type='text' placeholder='Enter text' />
+      <Test/>
+
+    </div>
+  )
+}
+export default Component;
