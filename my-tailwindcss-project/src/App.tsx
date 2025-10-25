5d25f557b7f9c09d3f454e7556876fe9f9a99d82
sunilnarravula <sunilnarravula2001@gmail.com>
2025-10-25 09:24:51 +0530

    day2 code

---
+++ my-tailwindcss-project/src/App.tsx
@@ -0,0 +1,12 @@
+ import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-red-500 text-3xl font-bold text-center mt-10">
+      Sunil Naidu
+    </div>
+  );
+}
+
+export default App;
